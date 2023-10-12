7809db3a772ea139d898bedc87ca348adf1c37be
rishabh <hespnod2348@gmai.com>
2023-10-12 14:53:59 +0530

    added MealsSummary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/Layout/NavBar";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <MealsSummary />
     </div>
   );
 }
