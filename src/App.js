96a3df7eb3b8d4cbb008573a05ab437559958bfd
rishabh <hespnod2348@gmai.com>
2023-10-13 16:30:54 +0530

    made the modal and overlay using module.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./components/Cart/Cart";
 import NavBar from "./components/Layout/NavBar";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <div>
+      <Cart />
       <NavBar />
       <main>
         <Meals />
--- src/components/Meals/MealsItem/MealsForm.jsx
+++ src/components/Meals/MealsItem/MealsForm.jsx
@@ -1,16 +1,6 @@
-import { useState } from "react";
 import Input from "../../UI/Input";
 
 const MealsForm = props => {
-    // const [quant, setQuant] = useState(0);
-    // const handleClick = () => {
-    //     if (quant >= 5) {
-    //         alert('Max allowed quantity is 5');
-    //     } else {
-    //         setQuant((prev) => prev + 1);
-    //     }
-    //     console.log(quant);
-    // }
     return (
         <form>
             <Input label='Amount' input={{ id: 'Amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }} />
